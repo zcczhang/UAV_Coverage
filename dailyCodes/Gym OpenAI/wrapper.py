b6af74b4a899f6fba1405cd7582f7f66e7e28248
zcczhang <2265981775@qq.com>
2020-06-05 11:58:37 -0500

    update gym api

---
+++ dailyCodes/Gym OpenAI/wrapper.py
@@ -0,0 +1,36 @@
+__author__ = "Charles Zhang"
+__time__ = "2020-06-05 11:16"
+
+import gym
+from typing import TypeVar
+import random
+
+Action = TypeVar('Action')
+
+
+class RandomActionWrapper(gym.ActionWrapper):
+    def __init__(self, env, epsilon=0.1):   # epsilon: probability of a random action
+        super(RandomActionWrapper, self).__init__(env)
+        self.epsilon = epsilon
+
+    def action(self, action: Action) -> Action:
+        if random.random() < self.epsilon:
+            print("Random!")
+            return self.env.action_space.sample()
+        return action
+
+
+if __name__ == "__main__":
+    env = RandomActionWrapper(gym.make("CartPole-v0"))
+
+    obs = env.reset()
+    total_reward = 0.0
+
+    while True:
+        obs, reward, done, _ = env.step(0)
+        total_reward += reward
+        if done:
+            break
+
+    print("Reward got: %.2f" % total_reward)
+ 
