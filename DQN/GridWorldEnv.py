ee70a094dc91067d39ae8665c1b875d01fd034ac
zcczhang <2265981775@qq.com>
2020-06-29 15:03:45 -0500

    Update GridWorldEnv.py

--- DQN/GridWorldEnv.py
+++ DQN/GridWorldEnv.py
@@ -1,15 +1,10 @@
-__author__ = "Charles Zhang"
-__time__ = "2020-06-29 14:35" 
-
 """
+Reference: 
 Reinforcement learning maze example.
 Red rectangle:          explorer.
 Black rectangles:       hells       [reward = -1].
 Yellow bin circle:      paradise    [reward = +1].
 All other states:       ground      [reward = 0].
-This script is the environment part of this example.
-The RL is in RL_brain.py.
-View more on my tutorial page: https://morvanzhou.github.io/tutorials/
 """
 import numpy as np
 import time
