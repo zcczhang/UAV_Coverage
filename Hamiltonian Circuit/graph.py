a6a4c48f18fdd32463377d67e3c2b4df79f49fcd
zcczhang <2265981775@qq.com>
2020-06-30 00:40:19 -0500

    update

---
+++ Hamiltonian Circuit/graph.py
@@ -0,0 +1,130 @@
+__author__ = "Charles Zhang"
+__time__ = "2020-06-29 23:26"
+
+
+class Graph:
+    def __init__(self, vertices):
+        self.graph = [[0 for column in range(vertices)]
+                      for row in range(vertices)]
+        self.V = vertices
+
+    '''
+    Check if this vertex is an adjacent vertex  
+    of the previously added vertex and is not  
+    included in the path earlier 
+    '''
+    def is_safe(self, v, pos, path):
+        # Check if current vertex and last vertex
+        # in path are adjacent
+        if self.graph[path[pos - 1]][v] == 0:
+            return False
+        # Check if current vertex not already in path
+        for vertex in path:
+            if vertex == v:
+                return False
+        return True
+
+    # A recursive tool function to solve hamiltonian circuit
+    def hc_tool(self, path, pos):
+        # base case: if all vertices are included in the path
+        if pos == self.V:
+            # Last vertex must be adjacent to the
+            # first vertex in path to make a cycle
+            if self.graph[path[pos - 1]][path[0]] == 1:
+                return True
+            else:
+                return False
+
+        # Try different vertices as a next candidate
+        # in Hamiltonian Circuit, exclude 0.
+        for v in range(1, self.V):
+            if self.is_safe(v, pos, path) is True:
+                path[pos] = v
+                if self.hc_tool(path, pos + 1) is True:
+                    return True
+                # Remove current vertex if it doesn't lead to a solution
+                path[pos] = -1
+        return False
+
+    def ham_circuit(self):
+        path = [-1] * self.V
+        ''' 
+        put vertex 0 as the first vertex in the path. 
+        If there is a Hamiltonian Circuit, then the path
+        can be started from any point of the cycle as 
+        the graph is undirected 
+        '''
+        path[0] = 0
+        if self.hc_tool(path, 1) is False:
+            print("Solution does not exist")
+            return False
+        self.print_solution(path)
+        return True
+
+    @staticmethod
+    def print_solution(path):
+        print("Solution Exists! \nHamiltonian Circuit Order:")
+        for vertex in path:
+            print(vertex, end=' ')
+        print(path[0])
+
+
+'''
+ Graph: 4x5 GridWorld
+ 
+      0 ——1 ——2 ——3 ——4
+      |   |   |   |   |
+      5 ——6 ——7 ——8 ——9
+      |   |   |   |   |
+      10——11——12——13——14
+      |   |   |   |   |
+      15——16——17——18——19   
+'''
+g1 = Graph(20)
+g1.graph = [[0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ],
+            [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, ],
+            [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, ],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0], ]
+
+# Solution
+g1.ham_circuit()
+
+'''
+    3x3 Grid World
+      0——1——2
+      |  |  |   
+      3——4——5 
+      |  |  |   
+      6——7——8
+      
+'''
+g2 = Graph(9)
+g2.graph = [[0, 1, 0, 1, 0, 0, 0, 0, 0],
+            [1, 0, 1, 0, 1, 0, 0, 0, 0],
+            [0, 1, 0, 0, 0, 1, 0, 0, 0, ],
+            [1, 0, 0, 0, 1, 0, 1, 0, 0],
+            [0, 1, 0, 1, 0, 1, 0, 1, 0],
+            [0, 0, 1, 0, 1, 0, 0, 0, 1, ],
+            [0, 0, 0, 1, 0, 0, 0, 1, 0],
+            [0, 0, 0, 0, 1, 0, 1, 0, 1],
+            [0, 0, 0, 0, 0, 1, 0, 1, 0], ]
+
+# Solution
+g2.ham_circuit()
+
